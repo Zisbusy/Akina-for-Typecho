f9845151b2c7f483924ced92ac459f0290dfabb4
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2020-03-14 21:08:52 +0800

    适配友情友链插件

--- Akina/links.php
+++ Akina/links.php
@@ -19,6 +19,20 @@
 	<span class="linkss-title"><?php $this->title() ?></span>
 	<article class="hentry">
 		<?php $this->content(); ?>
+		<br/>
+		<div class="links">
+		    <ul class="link-items fontSmooth">
+		    	<?php 
+				$rules ='<li class="link-item">
+						<a class="link-item-inner effect-apollo" href="{url}" title="{name}" target="_blank" >
+							<span class="sitename">{name}</span>
+							<div class="linkdes">{title}</div>
+						</a>
+					 </li>';
+			         Links_Plugin::output($pattern=$rules, $links_num=0, $sort=NULL);
+		         ?>
+		    </ul>
+		</div>
 	</article>
 </div>
 </div>
