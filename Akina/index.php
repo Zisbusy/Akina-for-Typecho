799d6dee0cdbe9bd031eb752aa22895f0b1b4c8d
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2020-03-16 18:10:34 +0800

    可自定义icon字段来改变文章列表头图

--- Akina/index.php
+++ Akina/index.php
@@ -170,7 +170,7 @@ if($sticky && $this->is('index') || $this->is('front')){
 		<?php else: ?>
 			<div class="post-entry">
 				<div class="feature">
-					<a href="<?php $this->permalink() ?>"><div class="overlay"><i class="iconfont">&#xe61e;</i></div><img src="<?php echo theurl.'images/random/deu'.mt_rand(1,7).'.jpg'; ?>"></a>
+					<a href="<?php $this->permalink() ?>"><div class="overlay"><i class="iconfont">&#xe61e;</i></div><img src="<?php if(array_key_exists('icon',unserialize($this->___fields()))){$this->fields->icon();}else{echo theurl.'images/random/deu'.mt_rand(1,7).'.jpg';}?>"></a>
 				</div>
 				<h1 class="entry-title"><a href="<?php $this->permalink() ?>"><?php $this->sticky(); $this->title() ?></a></h1>
 				<div class="p-time">
