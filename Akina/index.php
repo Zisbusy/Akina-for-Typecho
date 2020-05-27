da3dd80c1e82938ffa4917d2689f9d4cd03048f6
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2020-05-27 10:52:51 +0800

    首页大图调用函数

--- Akina/index.php
+++ Akina/index.php
@@ -46,7 +46,7 @@ if($this->options->sticky){
 <div class="blank"></div>
 	<div class="headertop">
 		<!-- 首页大图 -->
-		<div id="centerbg" style="background-image: url(<?php if(substr($this->options->headimg,0,1)=="i"){echo theurl,$this->options->headimg;}else{$this->options->headimg();}?>);">
+		<div id="centerbg" style="background-image: url(<?php authorProfile($this->options->headimg,theurl);?>);">
 			<!-- 左右倾斜 -->
 			<div class="slant-left"></div>
 			<div class="slant-right"></div>
