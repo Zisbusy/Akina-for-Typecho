77c01c99eeb35eabfa8526b2163cec5bc2893571
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2020-09-09 22:03:09 +0800

    对国内多核浏览器做优化

--- Akina/header.php
+++ Akina/header.php
@@ -13,6 +13,7 @@ define("theprofile", authorProfile($this->options->profile,theurl));
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="renderer" content="webkit">
 	<meta http-equiv="Cache-Control" content="no-transform" />
 	<meta http-equiv="Cache-Control" content="no-siteapp" />
 	<meta http-equiv="x-dns-prefetch-control" content="on" />
