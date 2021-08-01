35c20893f59730ddac2e35aaf059382983006fa0
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2021-08-01 10:21:23 +0800

    优化标签云头部图逻辑

--- Akina/tags.php
+++ Akina/tags.php
@@ -8,13 +8,28 @@
 ?>
 <div class="blank"></div>
 <div class="headertop"></div>
-<?php if (array_key_exists('thumbnail',unserialize($this->___fields())) & $this->fields->thumbnail != null): ?>
-	<div class="pattern-center">
-		<div class="pattern-attachment-img" style="background-image: url(<?php $this->fields->thumbnail(); ?>)"></div>
-		<header class="pattern-header"><h1 class="entry-title"><?php $this->title() ?></h1></header>
-	</div>
-	<style> @media (max-width: 860px){.site-main {padding-top: 30px;}} </style>
-<?php endif; ?>
+<?php 
+    if ( $this->fields->radioPostImg != 'none' && $this->fields->radioPostImg != null ) {
+        $bgImgUrl = '';
+        switch ( $this->fields->radioPostImg ) {
+        case 'custom':
+            $bgImgUrl = $this->fields->thumbnail;
+            break;
+        case 'random':
+            $bgImgUrl = theurl.'images/postbg/'.mt_rand(1,3).'.jpg';
+            break;
+        }
+        echo('
+            <div class="pattern-center">
+                <div class="pattern-attachment-img" style="background-image: url('.$bgImgUrl.')"></div>
+                    <header class="pattern-header">
+                <h1 class="entry-title">'.$this->title.'</h1>
+            </header>
+            </div>
+            <style> @media (max-width: 860px){.site-main {padding-top: 30px;}} </style>
+        ');
+    }
+?>
 <div id="content" class="site-content">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
