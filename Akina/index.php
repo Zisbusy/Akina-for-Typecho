0fb76597a04c536b706696dfbaf24639cd1e9008
Zisbusy <Zisbusy@outlook.com>
2022-04-03 09:42:39 +0800

    评论置兼容Typecho 1.2

--- Akina/index.php
+++ Akina/index.php
@@ -9,37 +9,37 @@
  */
  if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  $this->need('header.php');
- /** 文章置顶 */
+// 文章置顶
 if($this->options->sticky){
 	$sticky = $this->options->sticky; //置顶的文章cid，按照排序输入, 请以半角逗号或空格分隔
 	if($sticky && $this->is('index') || $this->is('front')){
-	    $sticky_cids = explode(',', strtr($sticky, ' ', ','));//分割文本 
-	    $sticky_html = "<span style='color:#ff6d6d;font-weight:600'>[置顶] </span>"; //置顶标题的 html css
-	    $db = Typecho_Db::get();
-	    $pageSize = $this->options->pageSize;
-	    $select1 = $this->select()->where('type = ?', 'post');
-	    $select2 = $this->select()->where('type = ? AND status = ? AND created < ?', 'post','publish',time());
-	    //清空原有文章的列队
-	    $this->row = [];
-	    $this->stack = [];
-	    $this->length = 0;
-	    $order = '';
-	    foreach($sticky_cids as $i => $cid) {
-		if($i == 0) $select1->where('cid = ?', $cid);
-		else $select1->orWhere('cid = ?', $cid);
-		$order .= " when $cid then $i";
-		$select2->where('table.contents.cid != ?', $cid); //避免重复
-	    }
-	    if ($order) $select1->order(null,"(case cid$order end)"); //置顶文章的顺序 按 $sticky 中 文章ID顺序
-	    if ($this->_currentPage == 1) foreach($db->fetchAll($select1) as $sticky_post){ //首页第一页才显示
-		$sticky_post['sticky'] = $sticky_html;
-		$this->push($sticky_post); //压入列队
-	    }
-		$uid = $this->user->uid; //登录时，显示用户各自的私密文章
-	    if($uid) $select2->orWhere('authorId = ? AND status = ?',$uid,'private');
-	    $sticky_posts = $db->fetchAll($select2->order('table.contents.created', Typecho_Db::SORT_DESC)->page($this->_currentPage, $this->parameter->pageSize));
-	    foreach($sticky_posts as $sticky_post) $this->push($sticky_post); //压入列队
-	    $this->setTotal($this->getTotal()-count($sticky_cids)); //置顶文章不计算在所有文章内
+    $sticky_cids = explode(',', strtr($sticky, ' ', ','));//分割文本 
+    $sticky_html = "<span style='color:#ff6d6d;font-weight:600'>[置顶] </span>"; //置顶标题的 html css
+    $db = Typecho_Db::get();
+    $pageSize = $this->options->pageSize;
+    $select1 = $this->select()->where('type = ?', 'post');
+    $select2 = $this->select()->where('type = ? AND status = ? AND created < ?', 'post','publish',time());
+    //清空原有文章的列队
+    $this->row = [];
+    $this->stack = [];
+    $this->length = 0;
+    $order = '';
+    foreach($sticky_cids as $i => $cid) {
+      if($i == 0) $select1->where('cid = ?', $cid);
+      else $select1->orWhere('cid = ?', $cid);
+      $order .= " when $cid then $i";
+      $select2->where('table.contents.cid != ?', $cid); //避免重复
+    }
+    if ($order) $select1->order('', "(case cid$order end)"); //置顶文章的顺序 按 $sticky 中 文章ID顺序
+    if (($this->_currentPage || $this->currentPage) == 1) foreach($db->fetchAll($select1) as $sticky_post){ //首页第一页才显示
+      $sticky_post['sticky'] = $sticky_html;
+      $this->push($sticky_post); //压入列队
+    }
+    $uid = $this->user->uid; //登录时，显示用户各自的私密文章
+    if($uid) $select2->orWhere('authorId = ? AND status = ?',$uid,'private');
+    $sticky_posts = $db->fetchAll($select2->order('table.contents.created', Typecho_Db::SORT_DESC)->page($this->_currentPage, $this->parameter->pageSize));
+    foreach($sticky_posts as $sticky_post) $this->push($sticky_post); //压入列队
+    $this->setTotal($this->getTotal()-count($sticky_cids)); //置顶文章不计算在所有文章内
 	}
 }
 ?>
