b4d7112f735df3d50c45ae02cd8297410cf7f0a6
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2022-06-09 21:04:00 +0800

    Update global.js

--- Akina/js/global.js
+++ Akina/js/global.js
@@ -1,4 +1,4 @@
-﻿//ajax评论
+//ajax评论
 var ajaxcomments = function(){
 	var
 	   $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
@@ -57,66 +57,61 @@ var ajaxcomments = function(){
             url: $(this).attr('action'),
             type: $(this).attr('method'),
             data: $(this).serializeArray(),async: true,
-            error: function() {
+            error: function(date) {
                 $("#submit").val("提交失败");
+                let msg = $(".container", date.responseText).prevObject[7].innerHTML.replace(/\s*/g,"");
+                $(document).ready(function(){
+                    $.alert("提示",msg);
+                });
+                setTimeout(function(){ $("#submit").val("再次提交"); }, 1000);
                 return false;
             },
             success: function(data) { //成功取到数据
                 try {
-                    if (!$(comment_list, data).length) {
-                        $("#comment-author-info").show();
-                        var msg =$(data)[7].innerHTML.replace(/\s*/g,"");
-                        $(document).ready(function(){
-                            $.alert("提示",msg);
-                        });
-                        setTimeout(function(){ $("#submit").val("再次提交"); }, 500);
-                        return false;
+                    new_id = $(comment_list, data).html().match(/id=\"?comment-\d+/g).join().match(/\d+/g).sort(function(a, b) {
+                        return a - b
+                    }).pop(); // TODO：找新 id，如果在第二页评论的话，找到的ID是有问题的！
+
+                    if ($('.page-navigator .prev').length && parent_id == ""){
+                        new_id = '';
+						var dd=$(".prev a").attr("href");//获取上页地址
+						$(".prev a").attr("href",""); //将地址清空
+						dd=dd.replace(/comment-page-(.*?)#comments/, "comment-page-1#comments");//将获取的地址页码改为1
+						$(".prev a").attr("href",dd); //将地址放回去
+						$('.prev a').get(0).click(); //点击这个超链接
+					}//判断当前评论列表是否在第一页,并且只会在母评论时候才会生效
+
+                    //插入评论内容到当前页面
+                    if (parent_id) {
+                        data = $('#li-comment-' + new_id, data).hide(); // 取新评论
+                        if ($('#' + parent_id).find(".children").length <= 0) {
+                            $('#' + parent_id).append("<div class='children'><ol class='comment-list'></ol></div>");
+                        }
+                        if (new_id)//new_id不为空的时候才会插入
+                            $('#' + parent_id + " .children .comment-list").prepend(data);
+                        parent_id = '';
                     } else {
-                        new_id = $(comment_list, data).html().match(/id=\"?comment-\d+/g).join().match(/\d+/g).sort(function(a, b) {
-                            return a - b
-                        }).pop(); // TODO：找新 id，如果在第二页评论的话，找到的ID是有问题的！
-
-                        if ($('.page-navigator .prev').length && parent_id == ""){
-                            new_id = '';
-							var dd=$(".prev a").attr("href");//获取上页地址
-							$(".prev a").attr("href",""); //将地址清空
-							dd=dd.replace(/comment-page-(.*?)#comments/, "comment-page-1#comments");//将获取的地址页码改为1
-							$(".prev a").attr("href",dd); //将地址放回去
-							$('.prev a').get(0).click(); //点击这个超链接
-						}//判断当前评论列表是否在第一页,并且只会在母评论时候才会生效
-
-                        //插入评论内容到当前页面
-                        if (parent_id) {
-                            data = $('#li-comment-' + new_id, data).hide(); // 取新评论
-                            if ($('#' + parent_id).find(".children").length <= 0) {
-                                $('#' + parent_id).append("<div class='children'><ol class='comment-list'></ol></div>");
-                            }
-                            if (new_id)//new_id不为空的时候才会插入
-                                $('#' + parent_id + " .children .comment-list").prepend(data);
-                            parent_id = '';
-                        } else {
-                            data = $('#li-comment-' + new_id, data).hide(); // 取新评论
-                            //console.log('该评论为母评论');
-                            if (!$(comment_list).length) //如果一条评论也没有的话
-                                $(respond).append('<ol class="comment-list"></ol>'); // 加 ol
-                            $(comment_list).prepend(data);
-                        }
-						setTimeout(function(){ $("#submit").val("提交成功"); }, 2000);
-                        $('#li-comment-' + new_id).fadeIn(); // 显示
-                        var num;
-                        $(comments).length ? (num = parseInt($(comments).text().match(/\d+/)), $(comments).html($(comments).html().replace(num, num + 1))) : 0;
-                        // 评论数加一
-                        TypechoComment.cancelReply();
-                        $(textarea).html('');$(textarea).val('');
-                        $(comment_reply + ' #cancel-comment-reply-link').unbind('click');
-                        click_bind(); // 新评论绑定
-                        if (new_id){
-                            $body.animate({scrollTop: $('#li-comment-' + new_id).offset().top - 200}, 900);
-                        }else{
-                            $body.animate({scrollTop: $('#comments').offset().top - 200}, 900);
-                        }
-						setTimeout(function(){ $("#submit").val("发表评论"); }, 3000);
+                        data = $('#li-comment-' + new_id, data).hide(); // 取新评论
+                        //console.log('该评论为母评论');
+                        if (!$(comment_list).length) //如果一条评论也没有的话
+                            $(respond).append('<ol class="comment-list"></ol>'); // 加 ol
+                        $(comment_list).prepend(data);
                     }
+					setTimeout(function(){ $("#submit").val("提交成功"); }, 2000);
+                    $('#li-comment-' + new_id).fadeIn(); // 显示
+                    var num;
+                    $(comments).length ? (num = parseInt($(comments).text().match(/\d+/)), $(comments).html($(comments).html().replace(num, num + 1))) : 0;
+                    // 评论数加一
+                    TypechoComment.cancelReply();
+                    $(textarea).html('');$(textarea).val('');
+                    $(comment_reply + ' #cancel-comment-reply-link').unbind('click');
+                    click_bind(); // 新评论绑定
+                    if (new_id){
+                        $body.animate({scrollTop: $('#li-comment-' + new_id).offset().top - 200}, 900);
+                    }else{
+                        $body.animate({scrollTop: $('#comments').offset().top - 200}, 900);
+                    }
+					setTimeout(function(){ $("#submit").val("发表评论"); }, 3000);
                 } catch(e) {
                     //alert('评论ajax错误!请截图并联系主题制作者！\n\n' + e);
                     window.location.reload();
@@ -476,4 +471,4 @@ if (doc != null) {
   }
 }
 //版本显示
-console.log("%cAkina for Typecho 4.1.2","background:#A0DAD0;color:#fff;margin:10px;padding:6px;border-radius:3px;","https://zhebk.cn");
+console.log("%cAkina for Typecho 4.1.3","background:#A0DAD0;color:#fff;margin:10px;padding:6px;border-radius:3px;","https://zhebk.cn");
